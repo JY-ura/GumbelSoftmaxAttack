d405c4977de6c827b9cdf8c7c773f003738ceedf
Yao Yanmeng <43411961+JY-ura@users.noreply.github.com>
2025-04-29 17:40:05 +0800

    Update __init__.py
    Remove the unused classes GumbelSoftmaxCustom

--- attacks/ours/probability_space/__init__.py
+++ attacks/ours/probability_space/__init__.py
@@ -2,6 +2,6 @@
 This module contains the implementation of the probability space attack.
 By optimizing the parameter of the probability, we turn the discrete categorical attack into continuous.
 """
-from .event_generator import GumbelSoftmaxCustom, GumbelSoftmaxTorch, TempretureSoftmax
+from .event_generator import GumbelSoftmaxTorch, TempretureSoftmax
 from .frame_generator import FrameGenerator
 from .probability_attack import ProbabilityAttacker
